36185977332df68ae76b19a66793e1f1e41c194a
Cristian Yepes <36001502+cyepes96@users.noreply.github.com>
2022-06-28 14:57:32 -0500

    Proyecto base
    Adicionando proyecto base

---
+++ src/main/java/com/test/mercadolibre/TestMercadoLibreApplication.java
@@ -0,0 +1,13 @@
+package com.test.mercadolibre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMercadoLibreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMercadoLibreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/mercadolibre/TestMercadoLibreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.mercadolibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMercadoLibreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
